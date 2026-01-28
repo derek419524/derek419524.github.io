32c7f48d6b0ed6d8d5115bbde8df81db3b2de69b
derek419524 <d41963113@gmail.com>
2026-01-28 16:53:49 +0000

    Begin DataShape

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -35,13 +35,28 @@ $(document).ready(function () {
   /////////////////////////////////////////////////
 
   // TODO 1: create a new shape object and add it to the array
-  
-
+  var shape = {
+    color: "blue",
+    shape: "circle",
+    repeat: 3,
+  };
+  dataShapes.push(shape)
   // TODO 2: add a new property to all data shapes
-  
-
+  for (var i = 0; i > dataShapes.length; i++) {
+  var currentShape = dataShapes[i];
+ if (currentShape.color === "red") {
+  goodBehavior = "bounce";
+  } else if (currentShape.color === "blue") {
+    goodBehavior = "blink";
+  } else {
+    goodBehavior = "spin";
+  }
+} 
   // TODO 3-a: add a function that handles the static display type
-  
+  function handleStatic(data) {
+  setBackgroundWithObject(data)
+  animationDetails.displayType = 1;
+  }
 
   // TODO 4-a: add a function that handles the good display type
   
