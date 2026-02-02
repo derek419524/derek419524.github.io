709bb347a7589a0e9c387eee6239c11deaf2c046
derek419524 <d41963113@gmail.com>
2026-02-02 16:45:50 +0000

    update

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -14,10 +14,10 @@ var score = 0; // variable to keep track of the score
 var started = false; // variable to keep track of whether the game has started
 
 // TODO 4, Part 1: Create the apple variable
-
+var apple = {};
 
 // TODO 5, Part 1: Create the snake variable
-
+var snake = {};
 
 // Constant Variables
 var ROWS = 20;
@@ -51,7 +51,7 @@ function init() {
   
   
   // TODO 4, Part 3: initialize the apple
-
+ makeApple()
 
   // TODO 6, Part 1: Initialize the interval
 
@@ -207,9 +207,11 @@ function endGame() {
  */
 function makeApple() {
   // TODO 4, Part 2: Fill in this function's code block
-
-
-
+ apple.element = $("<div>").addClass("apple").appendTo(board);
+ var randomPosition = getRandomAvailablePosition();
+ apple.row = randomPosition.row;
+ apple.column = randomPosition.column;
+ repositionSquare(apple);
 }
 
 /* Create an HTML element for a snakeSquare using jQuery. Then, given a row and
